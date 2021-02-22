d3f2fd67adc58c4acb1ef1c750d0984fbbf3e1ea
Fernanda Guzman Gomez <gzmanfer@gmail.com>
2021-02-22 14:34:36 -0600

    Commit

--- ch02-linked-lists/09-loop-node-detection.py
+++ ch02-linked-lists/09-loop-node-detection.py
@@ -11,10 +11,29 @@
     Output:     C
 
 @   Template:
-    01.
+    01. We create a dictionary of seen_nodes
+    02. Comparate new nodes with seen_nodes
     
 
 @   Explanation:
-    https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+    https://github.com/w-hat/ctci-solutions/blob/master/ch-02-linked-lists/08-loop-detection.py
 
 """
+
+# Given:
+#class ListNode:
+#    def __init__(self, x):
+#        self.val = x
+#        self.next = None
+
+
+# Approach 01: Hash Table
+def loopNode(self, head):
+    seen_nodes = {}
+
+    while head is not None:
+        if head in seen_nodes:
+            return head
+        seen_nodes[head] = True
+        head = head.next
+    return None
