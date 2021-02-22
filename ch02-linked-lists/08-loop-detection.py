383664828946ab1c77bfa8bc74ed3e2755a375af
Fernanda Guzman Gomez <gzmanfer@gmail.com>
2021-02-22 14:34:45 -0600

    Commit

--- ch02-linked-lists/08-loop-detection.py
+++ ch02-linked-lists/08-loop-detection.py
@@ -11,10 +11,42 @@
     Output:     True
 
 @   Template:
-    01.
-    
+    Approach 01: Hash Table
+    Approach 02: Floyd's Cycle Finding Algorithm
 
 @   Explanation:
     https://leetcode.com/problems/linked-list-cycle/
 
 """
+
+# Given:
+#class ListNode:
+#    def __init__(self, x):
+#        self.val = x
+#        self.next = None
+
+
+# Approach 01: Hash Table
+def hasCycle(self, head):
+    seen_nodes = set()
+
+    while head is not None:
+        if head in seen_nodes:
+            return True
+        seen_nodes.add(head)
+        head = head.next
+    return False
+
+
+# Approach 02: Floyd's Cycle Finding Algorithm
+def hasFloydCycle(self, head):
+    try:
+        slow = head
+        fast = head.next
+        while slow is not fast:
+            slow = slow.next
+            fast = fast.next.next
+        return True
+    
+    except:
+        return False
